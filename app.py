06f2c2cf3449de6ee32abe93caba4711dea96f96
py51 <py51@duke.edu>
2023-02-09 01:56:34 +0000

    submit

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ async def add(num1: int, num2: int):
 
     total = num1 + num2
     return {"total": total}
-
+    
+     
+     
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
